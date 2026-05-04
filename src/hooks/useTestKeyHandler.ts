87c56b8634cc2767b2dad001b7cc864071530da5
andrii kaliuha <andriikaliuha@gmail.com>
2026-05-04 23:05:11 +0300

    v0.643

--- src/hooks/useTestKeyHandler.ts
+++ src/hooks/useTestKeyHandler.ts
@@ -8,19 +8,51 @@ type UseTestKeyHandlerProps = {
   handleKeyDown: (event: KeyboardEvent) => void;
 };
 
+// export const useTestKeyHandler = ({ textContainerRef, inputRef, testStatus, handleKeyDown }: UseTestKeyHandlerProps) => {
+//   useEffect(() => {
+//     const textContainer = textContainerRef.current;
+//     const input = inputRef.current;
+
+//     if (testStatus === "running" || testStatus === "idle") {
+//       input?.addEventListener("keydown", handleKeyDown);
+//       input?.focus();
+//       textContainer?.addEventListener("click", () => input?.focus());
+//     }
+
+//     return () => {
+//       input?.removeEventListener("keydown", handleKeyDown);
+//     };
+//   }, [testStatus, handleKeyDown, textContainerRef, inputRef]);
+// };
+
 export const useTestKeyHandler = ({ textContainerRef, inputRef, testStatus, handleKeyDown }: UseTestKeyHandlerProps) => {
   useEffect(() => {
     const textContainer = textContainerRef.current;
     const input = inputRef.current;
 
+    const handleInput = (e: Event) => {
+      const inputEvent = e as InputEvent;
+      if (inputEvent.data) {
+        const syntheticEvent = new KeyboardEvent("keydown", { key: inputEvent.data });
+        handleKeyDown(syntheticEvent);
+        if (input) input.value = "";
+      }
+    };
+
+    const handleInputBackspace = (e: KeyboardEvent) => {
+      if (e.key === "Backspace") handleKeyDown(e);
+    };
+
     if (testStatus === "running" || testStatus === "idle") {
-      input?.addEventListener("keydown", handleKeyDown);
+      input?.addEventListener("input", handleInput);
+      input?.addEventListener("keydown", handleInputBackspace);
       input?.focus();
       textContainer?.addEventListener("click", () => input?.focus());
     }
 
     return () => {
-      input?.removeEventListener("keydown", handleKeyDown);
+      input?.removeEventListener("input", handleInput);
+      input?.removeEventListener("keydown", handleInputBackspace);
     };
   }, [testStatus, handleKeyDown, textContainerRef, inputRef]);
 };
